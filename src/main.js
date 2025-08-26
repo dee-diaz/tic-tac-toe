3634013ca119f5f363354d0b8035588656dd4a92
dee-diaz <diazthediablo@yahoo.com>
2025-08-26 11:06:12 -0300

    Winning conditions

--- src/main.js
+++ src/main.js
@@ -53,25 +53,100 @@ const gameController = (function () {
   };
   const player1 = createPlayer("Diana", SYMBOL.X);
   const player2 = createPlayer("Computer", SYMBOL.O);
-  let nextTurn = (player1.symbol === SYMBOL.X) ? player1.name : player2.name; // The player using the 'X' symbol always goes first
+  const playerX = player1.symbol === SYMBOL.X ? player1 : player2;
+  const playerO = player1.symbol === SYMBOL.O ? player1 : player2;
+  let nextTurn = playerX.name; // The player using the 'X' symbol always goes first
 
   function switchPlayer() {
-    (nextTurn === player1.name) ? nextTurn = player2.name : nextTurn = player1.name;
+    nextTurn === player1.name
+      ? (nextTurn = player2.name)
+      : (nextTurn = player1.name);
   }
 
   function playRound() {
-    const isGameboardFull = gameboard.isFull();
-    if (!isGameboardFull) {
-      if (nextTurn === player1.name) {
-        player1.makeMove(2);
-        console.log(`${player1.name} made a move`);
-      } else {
-        player2.makeMove(2);
-        console.log(`${player2.name} made a move`);
-      }
-      switchPlayer();
+    if (nextTurn === player1.name) {
+      player1.makeMove(2);
+      console.log(`${player1.name} made a move`);
     } else {
-      console.log("Game over");
+      player2.makeMove(2);
+      console.log(`${player2.name} made a move`);
+    }
+    switchPlayer();
+    console.log(`Next turn: ${nextTurn}`);
+    checkGameStatus();
+  }
+
+  function checkGameStatus() {
+    const isGameboardFull = gameboard.isFull();
+    const board = gameboard.getBoard();
+    const rows = [];
+    const cols = [];
+    const diagonals = [];
+    const xWin = SYMBOL.X.repeat(3);
+    const oWin = SYMBOL.O.repeat(3);
+
+    // Rows
+    for (let i = 0; i < board.length; i++) {
+      if (i === 0 || i === 3 || i === 6) rows.push([]);
+      if (i <= 2) rows[0].push(board[i]);
+      if (i > 2 && i <= 5) rows[1].push(board[i]);
+      if (i > 5 && i <= 8) rows[2].push(board[i]);
+    }
+
+    // Columns
+    for (let i = 0; i < board.length; i++) {
+      if (i === 0 || i === 1 || i === 2) cols.push([]);
+      if (i === 0 || i % 3 === 0) cols[0].push(board[i]);
+      if (i === 1 || (i - 1) % 3 === 0) cols[1].push(board[i]);
+      if (i === 2 || (i - 2) % 3 === 0) cols[2].push(board[i]);
+    }
+
+    // Diagonals
+    for (let i = 0; i < board.length; i++) {
+      if (i === 0 || i === 2) diagonals.push([]);
+      if (i === 0 || i % 4 === 0) diagonals[0].push(board[i]);
+      if (i === 2 || i === 4 || i === 6) diagonals[1].push(board[i]);
+    }
+
+    rows.forEach((row) => {
+      const str = row.join("");
+
+      if (str === xWin) {
+        defineWinner(playerX);
+      } else if (str === oWin) {
+        defineWinner(playerO);
+      }
+    });
+
+    cols.forEach((col) => {
+      const str = col.join("");
+
+      if (str === xWin) {
+        defineWinner(playerX);
+      } else if (str === oWin) {
+        defineWinner(playerO);
+      }
+    });
+
+    diagonals.forEach((diagonal) => {
+      const str = diagonal.join("");
+
+      if (str === xWin) {
+        defineWinner(playerX);
+      } else if (str === oWin) {
+        defineWinner(playerO);
+      }
+    });
+
+    if (isGameboardFull) defineWinner();
+  }
+
+  function defineWinner(player) {
+    if (!player) {
+      console.log("It's a tie!");
+    } else {
+      const winner = player.name;
+      console.log(winner);
     }
   }
 
